9e37caefe74c4c740cb3a7267d55db0d7841ef95
spencermcnamara <smcn0012@student.monash.edu>
2024-02-07 14:33:03 +1100

    Added comments to omega encoding

--- Elias (Omega) Encoding/Elias.py
+++ Elias (Omega) Encoding/Elias.py
@@ -1,14 +1,15 @@
 def elias(number):
+    # Converting from dec to bin
     if number == 0:
         return '1'
     bin_num = bin(number + 1)[2:]
-
+    # the recursive function
     def get_length_component(binary):
         new_lc_segment = bin(len(binary) - 1)[2:]
-        if new_lc_segment == '1':
+        if new_lc_segment == '1': # base case
             return "0"
         new_lc_segment = "0" + new_lc_segment[1:]
-        return get_length_component(new_lc_segment) + new_lc_segment
+        return get_length_component(new_lc_segment) + new_lc_segment # appending the length component
     
     return get_length_component(bin_num) + bin_num
 
