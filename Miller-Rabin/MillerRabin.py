6782def8953c985b6caefd52431cf0671e8cfb73
spencermcnamara <smcn0012@student.monash.edu>
2024-03-08 11:09:38 +1100

    Improved Miller Rabin by not storing all the mod results and instead only store the previous mod result

--- Miller-Rabin/MillerRabin.py
+++ Miller-Rabin/MillerRabin.py
@@ -10,7 +10,7 @@ def binToDec(binary):
 def MillerRabin(number, accuracy):
     # check for even
     t = bin(number - 1)
-    if t[-1] == 1:
+    if t[-1] == "1":
         return False
 
     # Factorise n - 1 into 2^s * t
@@ -25,26 +25,23 @@ def MillerRabin(number, accuracy):
         # Chose a witness get the first modulo
         witness = random.randint(2, number - 2)
         current_mod_result = (witness ** t) % number
-        mod_results = [1 for _ in range(shift + 1)]
-        mod_results[0] = current_mod_result
+        previous_mod_result = 0
 
         # Recursively calculate the next modulo using the repeated square method
         for i in range(1, shift + 1):
+            previous_mod_result = current_mod_result
             current_mod_result = (current_mod_result ** 2) % number
-            mod_results[i] = current_mod_result
             if current_mod_result == 1:
-                break
+                # Case 2: The previous mod result wasnt congruent to +/-1 mod(number) 
+                if previous_mod_result != 1 and previous_mod_result != number - 1:
+                    return False
+                else:
+                    break
 
         # Case 1: a^{n-1} not congruent to 1 mod(number) 
         if current_mod_result != 1: 
             return False
-        
 
-        for i in range(1, shift):
-            # Case 2: The previous mod result wasnt congruent to +/-1 mod(number) 
-            if mod_results[i] == 1 and mod_results[i-1] != 1 and mod_results[i-1] != number - 1:
-                return False
-        
         # Case 3/4: Probably Prime
         counter += 1
 
